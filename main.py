f896ad1a69186bf66d1894b624fba4c8d883d6e8
sean mabli <70709738+seanmabli@users.noreply.github.com>
2022-03-16 17:48:18 +0000

    change example for youtube.com

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("http://www.youtube.com")
+elem = driver.find_element_by_class_name("search_query")
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+sleep(100)
 driver.close()
