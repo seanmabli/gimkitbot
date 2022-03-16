7f2213c5a21caf42216268617dd896bde7fa43f2
sean mabli <70709738+seanmabli@users.noreply.github.com>
2022-03-16 20:21:42 +0000

    make username and password a user input

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+username = "mablis25@lsrhs.net" # input('enter username: ')
+password = "alphabeta" # input('enter password: ')
+
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
@@ -14,13 +17,13 @@ driver.get("http://www.gimkit.com/hub")
 sleep(1)
 
 usernameinput = driver.find_element(by=By.CLASS_NAME, value="ant-input.ant-input-lg")
-usernameinput.send_keys("mablis25@lsrhs.net")
+usernameinput.send_keys(username)
 usernameinput.send_keys(Keys.RETURN)
 
 sleep(1)
 
 passwordinput = driver.find_element(by=By.CLASS_NAME, value="ant-input.ant-input-lg")
-passwordinput.send_keys("alphabeta")
+passwordinput.send_keys(password)
 passwordinput.send_keys(Keys.RETURN)
 
 sleep(100)
