a98974237ff70c6c3ca176e4c98a48ba4d24f800
SeanMabli <smdrone1@gmail.com>
2022-03-17 17:04:58 -0400

    get to work on win10

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from fake_useragent import UserAgent
 from time import sleep
 
 username = "mablis25@lsrhs.net" # input('enter username: ')
 password = "alphabeta" # input('enter password: ')
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.maximize_window()
 driver.get("http://www.gimkit.com/hub")
 
 sleep(1)
@@ -28,8 +27,13 @@ passwordinput.send_keys(Keys.RETURN)
 
 sleep(1)
 
-joincode = driver.find_element(by=By.CLASS_NAME, value="sc-jGFFOr.cKOjwg.flex.vc.hc")
-joincode.click()
+try:
+  joincode = driver.find_element(by=By.CLASS_NAME, value="sc-jGFFOr.cKOjwg.flex.vc.hc")
+  joincode.click()
+except:
+  quit()
 
-sleep(100)
+while True:
+  pass
+
 driver.close()
