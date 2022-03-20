205b0ddf27114631e0efcb102c8404077a83191c
SeanMabli <smdrone1@gmail.com>
2022-03-19 22:53:07 -0400

    work on answering questions

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
+answers = {}
+
 username = "mablis25@lsrhs.net" # input('enter username: ')
 password = "alphabeta" # input('enter password: ')
 
@@ -28,18 +30,36 @@ sleep(1)
 
 while True:
   try:
-    joincode = driver.find_element(by=By.CLASS_NAME, value="sc-jGFFOr.cKOjwg.flex.vc.hc")
-    joincode.click()
+    joincode = driver.find_element(by=By.CLASS_NAME, value="sc-jGFFOr.cKOjwg.flex.vc.hc").click()
   except:
     pass
   else:
     break
 
-humancontrol = True
 while True:
-  if humancontrol:
-    pass
+  try:
+    driver.find_element(by=By.CLASS_NAME, value="sc-eylKsO.denLGz.flex-column")
+  except:
+    humancontrol = True
   else:
-    pass
+    humancontrol = False
+
+  if humancontrol:
+    print('human')
+  else:
+    # print('bot')
+    question = driver.find_element(by=By.CLASS_NAME, value="sc-gHpXsY.criBYM").text
+    possibleanswers = [driver.find_elements(by=By.CLASS_NAME, value="sc-iDsUSg.cVPfqH"), driver.find_elements(by=By.CLASS_NAME, value="sc-iDsUSg.esDDTm"), driver.find_elements(by=By.CLASS_NAME, value="sc-iDsUSg.lgneBT"), driver.find_elements(by=By.CLASS_NAME, value="sc-iDsUSg.cpbUXN")]
+    # if question in answers.keys():
+    #   for i in range(4):
+    #     if answers[question] == possibleanswers[i].get_attribute('id'):
+    #       possibleanswers[i].click()
+    # else:
+    #   driver.find_elements(by=By.CLASS_NAME, value="sc-iDsUSg.cVPfqH").click()
+    #   sleep(100)
+    #   quit()
+
+    # sleep(1)
+    # driver.find_element(by=By.CLASS_NAME, value="anticon.anticon-close").click()
 
 driver.close()
